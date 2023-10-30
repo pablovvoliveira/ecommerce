21c812f2275aaf665b1de1aafadcb210963e208f
pablo <pablovvoliveira@gmail.com>
2023-10-30 11:24:08 -0300

    Payment entity

--- src/main/java/com/pablovvoliveira/ecommerce/entities/Order.java
+++ src/main/java/com/pablovvoliveira/ecommerce/entities/Order.java
@@ -9,7 +9,6 @@ import java.util.Objects;
 @Table(name = "tb_order")
 public class Order {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,14 +21,18 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, OrderStatus status, User client) {
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
         this.id = id;
         this.moment = moment;
         this.status = status;
         this.client = client;
+        this.payment = payment;
     }
 
     public Long getId() {
@@ -62,6 +65,14 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
