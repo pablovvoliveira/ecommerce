679666ecadce0cd97d4e0aff6dd56b9b907d3b14
pablo <pablovvoliveira@gmail.com>
2023-11-06 12:13:57 -0300

    GET products paged

--- src/main/java/com/pablovvoliveira/ecommerce/services/ProductService.java
+++ src/main/java/com/pablovvoliveira/ecommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.pablovvoliveira.ecommerce.dto.ProductDTO;
 import com.pablovvoliveira.ecommerce.entities.Product;
 import com.pablovvoliveira.ecommerce.respositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +22,11 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> products = repository.findAll(pageable);
+        return products.map(x -> new ProductDTO(x));
     }
 }
