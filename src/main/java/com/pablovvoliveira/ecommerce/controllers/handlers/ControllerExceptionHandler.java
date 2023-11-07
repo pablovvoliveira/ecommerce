8c14a5c85f222817d53bea6d19aced442ae16b3b
pablo <pablovvoliveira@gmail.com>
2023-11-06 23:13:14 -0300

    Exception handling

---
+++ src/main/java/com/pablovvoliveira/ecommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.pablovvoliveira.ecommerce.controllers.handlers;
+
+import com.pablovvoliveira.ecommerce.dto.CustomError;
+import com.pablovvoliveira.ecommerce.services.exceptions.DatabaseException;
+import com.pablovvoliveira.ecommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
